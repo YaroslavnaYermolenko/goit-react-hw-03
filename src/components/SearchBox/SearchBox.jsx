6938f51ed89032959da80fd964b98772d1dab5c7
YaroslavnaYermolenko <162818983+YaroslavnaYermolenko@users.noreply.github.com>
2025-01-02 21:12:22 +0200

    Update SearchBox.jsx

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -4,7 +4,7 @@ import css from "./SearchBox.module.css";
 export default function SearchBox({ inputValue, handleChange }) {
   return (
     <div className={css.searchBox}>
-      <h3>Find contacts by name {inputValue}</h3>
+      <h3>Find contacts by name</h3>
       <input
         type="text"
         value={inputValue ?? "Enter name to search"}
